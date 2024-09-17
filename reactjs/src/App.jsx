b958d73e0abd5fe3bc2c0ba2b162bfe5120f62ea
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-09-17 22:44:00 +0530

    this is first commit

---
+++ reactjs/src/App.jsx
@@ -0,0 +1,11 @@
+import Cybrom from "./Cybrom";
+
+const App=()=>{
+return(
+    <div>
+    <h1>Cybrom Pvt Ldt</h1>
+    <Cybrom/>
+    </div>
+)
+}
+export default App;
---
+++ reactjs/src/Cybrom.jsx
@@ -0,0 +1,8 @@
+const Cybrom = () => {
+  return (
+    <div>
+      <h1>Welcom React Class</h1>
+    </div>
+  );
+};
+export default Cybrom;
