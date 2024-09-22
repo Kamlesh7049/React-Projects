0c2c2a70aaab53a716c4a49e4a3c6148b447bea1
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-09-22 13:54:06 +0530

    added Inline css & External css

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,43 +1,48 @@
+
 // import Bhopal from "./Bhopal";
 // import Code from "./Code";
 // import Cybrom from "./Cybrom";
 
-import Students from "./Students";
-import College from "./College";
-import Developers from "./Developer";
+// import Students from "./Students";
+// import College from "./College";
+// import Developers from "./Developer";
+import Homework from "./Homework";
+import Engineer from "./Tcs";
 
 
 
-
-const student={
-    "rollno":111,
-    "name":"Kamlesh",
-    "city":"indore",
-    "fees":1000
-}
-const App=()=>{
-return(
-    <>
+// Props
+// ---------------------
+// const student={
+//     "rollno":111,
+//     "name":"Kamlesh",
+//     "city":"indore",
+//     "fees":1000
+// }
+// const App=()=>{
+// return(
+    // <>
     {/* <h1>Cybrom Pvt Ldt</h1> */}
     {/* <Cybrom/> */}
     {/* <Bhopal/> */}
     {/* <Code/> */}
-    <h1>Welcom!!!</h1>
+    // <h1>Welcom!!!</h1>
 
+     {/* <Students nm="" rno="230" ct="bhopal"/> */}
 
     {/* <College nm="TIT" Seats="230" ct="bhopal"/> */}
-    <Developers 
+    {/* <Developers 
     sturno={student.rollno}
     stunm={student.name}
-    stuct={student.city}
-    stufs={student.fees}
-    />
+    stuct={student.city} */}
+    {/* stufs={student.fees} */}
+    {/* />  */}
 
 
-    </>
-)
-}
-export default App;
+    {/* </> */}
+{/* ) */}
+{/* } */}
+// export default App;
 
 // classs com  fun comp
 
@@ -97,3 +102,76 @@ export default App;
 
 
 
+
+
+//Inline CSS
+// -------------
+// const App=()=>{   
+//     return(
+//         <>
+//         <h1 style={{color:"red",fontFamily:"arial"}}>Welcom  to Cybrom</h1>
+//         </>
+//     )
+//     }
+//     export default App;
+
+
+// const App=()=>{   
+//     return(
+//         <>
+//         <h1 style={{color:"green",fontFamily:"verdana"}}>Welcom  to Cybrom !!!</h1>
+//         <h1 style={{color:"red",fontFamily:"arial"}}>We are Developer !!</h1>
+//         <div style={{color:"blue",fontSize:"20px"}}>We are working MA</div>
+//         </>
+//     )
+//     }
+//     export default App;
+
+// const data={
+//     color:"red",
+//     textDecoration:"underline",
+// }
+// const data1={
+//     color:"blue",
+// }
+
+// const App=()=>{   
+//     return(
+//         <>
+//         <h1 
+//         style={data}>Welcom  to Cybrom</h1>
+//         <h2 style={data1}>We are Engineer!!</h2>
+//         </>
+//     )
+//     }
+//     export default App;
+
+
+
+// External CSS
+// -------------------
+
+// const App=()=>{   
+//     return(
+//         <>
+//         <h1>Welcom Cybrom!!</h1>
+//         <Engineer/>
+//         </>
+//     )
+//     }
+//     export default App;
+
+
+
+const App=()=>{   
+    return(
+        <>
+
+        
+        {/* <h1>Welcom to Cybrom</h1> */}
+        <Homework/>
+        
+        </>
+    )
+    }
+    export default App;
---
+++ reactjs/src/Tcs.jsx
@@ -0,0 +1,10 @@
+const Engineer=()=>{
+    return(
+      <>
+      <h1 className="data">WE are Software Engineer</h1>
+      <h1 id="data1"> WE are MARN Stack</h1>
+      
+      </>
+    );
+  };
+  export default Engineer;
