79cf09dc1dc4666bb726d4768c07372bb21788b4
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-09-23 21:16:15 +0530

    added botstrap

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,166 +1,6 @@
-
-// import Bhopal from "./Bhopal";
-// import Code from "./Code";
-// import Cybrom from "./Cybrom";
-
-// import Students from "./Students";
-// import College from "./College";
-// import Developers from "./Developer";
-import Homework from "./Homework";
-import Engineer from "./Tcs";
-
-
-
-// Props
-// ---------------------
-// const student={
-//     "rollno":111,
-//     "name":"Kamlesh",
-//     "city":"indore",
-//     "fees":1000
-// }
-// const App=()=>{
-// return(
-    // <>
-    {/* <h1>Cybrom Pvt Ldt</h1> */}
-    {/* <Cybrom/> */}
-    {/* <Bhopal/> */}
-    {/* <Code/> */}
-    // <h1>Welcom!!!</h1>
-
-     {/* <Students nm="" rno="230" ct="bhopal"/> */}
-
-    {/* <College nm="TIT" Seats="230" ct="bhopal"/> */}
-    {/* <Developers 
-    sturno={student.rollno}
-    stunm={student.name}
-    stuct={student.city} */}
-    {/* stufs={student.fees} */}
-    {/* />  */}
-
-
-    {/* </> */}
-{/* ) */}
-{/* } */}
-// export default App;
-
-// classs com  fun comp
-
-// const name="Ankita";
-// const age=30;
-
-// const App=()=>{
-//     return(
-        
-//         <p>Welcom to {name} cybrom age:{age}my age{age*3}</p>
-        
-    
-//     )
-//     }
-//     export default App;
-
-// const Sub=(<ol>
-//     <li>Php</li>
-//     <li>java</li>
-//     <li>python</li>
-//     <li>css</li>
-//     <li>html</li>
-// </ol>)
-
-// const App=()=>{   
-//     return(
-//         <div>
-//         <h1>Sub:{Sub}</h1>
-//         </div>
-//     )
-//     }
-//     export default App;
-
-// const App=()=>{   
-//     return(
-//         <>// extra node nhi banega
-//         <h1>Sub</h1>
-//         </>
-//     )
-//     }
-//     export default App;
-
-
-
-// const App=()=>{   
-//     return(
-//         <>
-//         <h1 className="data">form</h1>
-//         Enter name:<input type="text"/><br />
-//        Enter City :<input type="text"/>
-//        <hr />
-//         </>
-//     )
-//     }
-//     export default App;
-
-
-
-
-
-
-//Inline CSS
-// -------------
-// const App=()=>{   
-//     return(
-//         <>
-//         <h1 style={{color:"red",fontFamily:"arial"}}>Welcom  to Cybrom</h1>
-//         </>
-//     )
-//     }
-//     export default App;
-
-
-// const App=()=>{   
-//     return(
-//         <>
-//         <h1 style={{color:"green",fontFamily:"verdana"}}>Welcom  to Cybrom !!!</h1>
-//         <h1 style={{color:"red",fontFamily:"arial"}}>We are Developer !!</h1>
-//         <div style={{color:"blue",fontSize:"20px"}}>We are working MA</div>
-//         </>
-//     )
-//     }
-//     export default App;
-
-// const data={
-//     color:"red",
-//     textDecoration:"underline",
-// }
-// const data1={
-//     color:"blue",
-// }
-
-// const App=()=>{   
-//     return(
-//         <>
-//         <h1 
-//         style={data}>Welcom  to Cybrom</h1>
-//         <h2 style={data1}>We are Engineer!!</h2>
-//         </>
-//     )
-//     }
-//     export default App;
-
-
-
-// External CSS
-// -------------------
-
-// const App=()=>{   
-//     return(
-//         <>
-//         <h1>Welcom Cybrom!!</h1>
-//         <Engineer/>
-//         </>
-//     )
-//     }
-//     export default App;
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 
 const App=()=>{   
@@ -168,10 +8,20 @@ const App=()=>{
         <>
 
         
-        {/* <h1>Welcom to Cybrom</h1> */}
-        <Homework/>
+        {/* <h1 style={{color:"green",fontFamily:"verdana", textAlign:"center",}}>Welcom to Cybrom</h1> */}
+        <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Cybrom Technology </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Service</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
         
-        </>
+       </>
     )
     }
     export default App;
