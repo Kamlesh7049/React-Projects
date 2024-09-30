7165eab6b22446d662383e87392e9c884bc8334a
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-09-30 23:14:15 +0530

    used Normal Funtion

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,18 +1,18 @@
-const APP = () => {
-  const subject = ["php", "oracle", "java", "asp", "html", "pyton"];
-  const ans = subject.map((key) => {
-    return (
+const App = () => {
+  const subject = ["php", "oracle", "java", "asp", "html", "python","Reactjs"];
+  // const ans = subject.map((key) => <li>{key}</li>);
+  const data=subject.map(function(data){
+    return(
       <>
-        <li>{key}</li>
+      <ul>{data}</ul>
       </>
-    );
-  });
+    )
+  })
   return (
     <>
-      <h1>Hello world</h1>
-      <ol>{ans}</ol>
+      <h1>Hello World</h1>
+      <ul>{data}</ul>
     </>
   );
 };
-
-export default APP;
+export default App;
