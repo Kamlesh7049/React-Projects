b49ad270ad428487f2780472b36c3371538bc479
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-10-02 23:52:58 +0530

    React-Event

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,29 +1,14 @@
-import EmpData from "./Map/EmpData";
-import EmpDesign from "./Map/EmpDesign";
-
-
-
-
-
 const App=()=>{
-  const ans=EmpData.map((key)=><EmpDesign empno={key.empno} empnm={key.name} dpt={key.dept} sal={key.salary}/>)
-
+  const display=()=>{
+    alert("This is my event!!");
+    
+  }
   return(
-
-  <>
-  <h1>Welcom Developers!!!</h1>
-  <table border="1px">
-   <tr>
-    <th>Emp Number</th>
-    <th>Emp Name</th>
-    <th>Emp dept</th>
-    <th>Emp Salary</th>
-   </tr>
-{ans}
-
-  </table>
-  </>
+    <>
+    
+    <h1>Welcome To  React Event !!</h1>
+    <button onClick={display}>Click here</button>
+    </>
   )
-
 }
 export default App;
